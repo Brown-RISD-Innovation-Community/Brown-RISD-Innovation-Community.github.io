0a1fb7423a6a16d00f7ba3558d4e700b604c35b6
Ivan <ivanzhao8@gmail.com>
2020-08-19 02:10:51 -0400

    Updates

---
+++ precache-manifest.cff9905b333ee45411c41faac9fa7925.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd8253bd21565b27a4ae21bb4e48faa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b79c96295d3210bff50",
+    "url": "/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "33db036b074cadf11ef0",
+    "url": "/static/css/main.5df7d988.chunk.css"
+  },
+  {
+    "revision": "7b79c96295d3210bff50",
+    "url": "/static/js/2.86284d3b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.86284d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33db036b074cadf11ef0",
+    "url": "/static/js/main.52d15c75.chunk.js"
+  },
+  {
+    "revision": "84f0edecb7a0fef1a855",
+    "url": "/static/js/runtime-main.853e2038.js"
+  },
+  {
+    "revision": "8c9396e6e1a85515c20e1200db20881b",
+    "url": "/static/media/Niyoshi.8c9396e6.jpg"
+  },
+  {
+    "revision": "3e7bb151958a99f9914c5d6106468ffb",
+    "url": "/static/media/arrow.3e7bb151.png"
+  },
+  {
+    "revision": "361987f2d8daafbf1ce857af1dd660b3",
+    "url": "/static/media/aryan.361987f2.jfif"
+  },
+  {
+    "revision": "367d36782ffcc4398b5a86d4cb3beef4",
+    "url": "/static/media/audrey.367d3678.jfif"
+  },
+  {
+    "revision": "4255e47f8352b11c28b4c6dbf2ca6fa2",
+    "url": "/static/media/brie.4255e47f.jfif"
+  },
+  {
+    "revision": "ffc4ac81611b87236c2e8055e98d6fb7",
+    "url": "/static/media/christine.ffc4ac81.png"
+  },
+  {
+    "revision": "b917a60ebd3ebb4fd3b101409deb176b",
+    "url": "/static/media/ivan.b917a60e.png"
+  },
+  {
+    "revision": "46c65222b727b8a34b9bd0342b99a312",
+    "url": "/static/media/kia.46c65222.jpg"
+  },
+  {
+    "revision": "deb9ce7a9523b22118d17e0099029307",
+    "url": "/static/media/landing_design.deb9ce7a.svg"
+  },
+  {
+    "revision": "434e24fb8ab40ca0c1762ad6f1f62457",
+    "url": "/static/media/logo.434e24fb.svg"
+  },
+  {
+    "revision": "0d9c2454cb4746f84116b6b6a8104af8",
+    "url": "/static/media/new_logo.0d9c2454.svg"
+  },
+  {
+    "revision": "c4b34f1da5c7997fef64f0a4f81dc459",
+    "url": "/static/media/scissors.c4b34f1d.svg"
+  },
+  {
+    "revision": "0e2cabfc0a5e0bcdfd1fc290b6c203eb",
+    "url": "/static/media/stephanie.0e2cabfc.jfif"
+  },
+  {
+    "revision": "c1a263f3dcbb8f9d39332adcfc04996f",
+    "url": "/static/media/xinru.c1a263f3.jpg"
+  }
+]);

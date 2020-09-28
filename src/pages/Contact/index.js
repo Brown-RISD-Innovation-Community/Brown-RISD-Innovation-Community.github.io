0651a26ed1a99d996919a5fb818dc382d454feda
SorinCho <sorincho@gmail.com>
2020-09-28 16:17:49 -0400

    Responsibe contact

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -12,22 +12,19 @@ import {
   AiFillLinkedin,
 } from "react-icons/ai";
 
-const textStyle = {
-  fontSize: 32,
-};
-
 export default function index() {
   return (
     <div
-      style={{ backgroundColor: "#00BFA9", color: "#0B43CB", height: "80vh" }}
+      style={{ backgroundColor: "#00BFA9", color: "#0B43CB",  }}
     >
       <NavigationBar linkFontColor="white" />
       <Container className="pt-5 mb-5">
-        <Row className="font-weight-bold" style={textStyle}>
-          CONNECT WITH US ON SOCIAL MEDIA
+        <Row className="font-weight-bold">
+          <h2><strong>CONNECT WITH US ON SOCIAL MEDIA</strong></h2>
         </Row>
         <Row className="p-3">
-          <Col style={textStyle}>
+          <Col>
+            <h3>
             <a
               href="https://www.facebook.com/brownrisdinnovation/"
               target="_blank"
@@ -35,41 +32,44 @@ export default function index() {
               style={{ color: "#0B43Cb" }}
             >
               <AiFillFacebook className="mb-2" />
-              Facebook
+              <span className="d-none d-sm-inline">Facebook</span>
+              
+              
             </a>
+            </h3>
           </Col>
-          <Col style={textStyle}>
-            <a
+          <Col>
+            <h3><a
               href="https://www.instagram.com/brownrisdinnovation/"
               target="_blank"
               rel="noopener noreferrer"
               style={{ color: "#0B43Cb" }}
             >
               <AiFillInstagram className="mb-2" />
-              Instagram
-            </a>
+              <span className="d-none d-sm-inline">Instagram</span>
+            </a></h3>
           </Col>
-          <Col style={textStyle}>
-            <a
+          <Col>
+            <h3><a
               href="https://www.linkedin.com/company/brownrisdinnovation/"
               target="_blank"
               rel="noopener noreferrer"
               style={{ color: "#0B43Cb" }}
             >
               <AiFillLinkedin className="mb-2" />
-              Linkedin
-            </a>
+              <span className="d-none d-sm-inline">LinkedIn</span>
+            </a></h3>
           </Col>
         </Row>
         {/* <Row>
           Email us @<a href="mailto:xinru_li@brown.edu">xinru_li@brown.edu</a>
         </Row> */}
       </Container>
-      <Container className="mt-5 pb-5">
-        <Row className="font-weight-bold" style={textStyle}>
-          Looking to partner with us?
+      <Container className="mt-5 pb-5 justify-content-sm-center justify-content-md-start">
+        <Row className="font-weight-bold">
+          <h2><strong>Looking to partner with us?</strong></h2>
         </Row>
-        <Row className="pt-3 pb-3" style={{ fontSize: 24 }}>
+        <Row className="pt-3 pb-3 px-3" style={{ fontSize: 24, wordBreak: "break-all" }}>
           Email us here:&nbsp;
           <a href="mailto:hello@brownrisdinnovation.com">
             hello@brownrisdinnovation.com

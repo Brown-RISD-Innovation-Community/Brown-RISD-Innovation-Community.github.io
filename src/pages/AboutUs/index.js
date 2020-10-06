dc0d97013d4c4652908ee6d698b897fcf90c0df5
SorinCho <sorincho@gmail.com>
2020-10-06 00:39:16 -0400

    About us refactor

--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -50,6 +50,20 @@ const bodyStyle = {
   textAlign: "left",
 };
 
+function Profile(props) {
+  return (
+    <Col xs={12} md={3} className="">
+      <img
+        src={props.src}
+        alt={`headshot of ${props.name}`}
+        style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
+      />
+      <div className="font-weight-bold pt-3">{props.name}</div>
+      <div>{props.year}</div>
+      <div>{props.major}</div>
+    </Col>
+  );
+}
 export default function index() {
   return (
     <div
@@ -59,80 +73,75 @@ export default function index() {
       <NavigationBar linkFontColor="white" />
       <Container className="pt-5 d-flex pb-5 ">
         <Row>
-        <Col xs={12} md={8}>
-          <Row >
-            <h1 style={textStyle}>OUR TEAM</h1>
-          </Row>
-          <Row className="pt-3 pb-3">
-            <div style={bodyStyle}>
-              BRIC BRINGS TOGETHER STUDENTS FROM{" "}
-              <bdi style={{ textDecoration: "underline" }}>
-                BROWN UNIVERSITY
-              </bdi>{" "}
-              AND <bdi style={{ textDecoration: "underline" }}>RISD</bdi>. OUR
-              INTERESTS RANGE FROM ENTREPRENEURSHIP AND HISTORY TO DESIGN AND
-              TECHNOLOGY.{" "}
-            </div>
-          </Row>
-          <Row>
-            <div className="pl-5" style={{ color: "#0B43CB", fontSize: 24 }}>
-              MEET OUR TEAM
-            </div>
-            <img
-              className="mt-3 ml-4"
-              src={Arrow}
-              alt="Arrow to meet the team"
-              style={{ width: "300px", maxWidth: "80%" }}
-            />
-          </Row>
-        </Col>
-        <Col xs={12} md={4}>
-          <img src={TeamIllustration} alt="Illustration for Team Page"
-            style={{ width: "350px", maxWidth: "100%" }}/>
-        </Col>
-        </Row>
-      </Container>
-      <Container style={{ backgroundColor: "white", borderRadius: "25px", maxWidth: "90%"}}>
-        <Row className="pl-5 pt-3 pb-3" style={bodyStyle}>
-          Co-Chairs
-        </Row>
-        <Row className="d-flex pb-5 justify-content-start">
-          <Col xs={12} md={3} className="">
-            <img
-              src={Ivan}
-              alt="headshot of Ivan"
-              style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
-            />
-            <div className="font-weight-bold pt-3">Ivan Zhao</div>
-            <div>Brown '22</div>
-            <div>APMA - CS</div>
+          <Col xs={12} md={8}>
+            <Row>
+              <h1 style={textStyle}>OUR TEAM</h1>
+            </Row>
+            <Row className="pt-3 pb-3">
+              <div style={bodyStyle}>
+                BRIC BRINGS TOGETHER STUDENTS FROM{" "}
+                <bdi style={{ textDecoration: "underline" }}>
+                  BROWN UNIVERSITY
+                </bdi>{" "}
+                AND <bdi style={{ textDecoration: "underline" }}>RISD</bdi>. OUR
+                INTERESTS RANGE FROM ENTREPRENEURSHIP AND HISTORY TO DESIGN AND
+                TECHNOLOGY.{" "}
+              </div>
+            </Row>
+            <Row>
+              <div className="pl-5" style={{ color: "#0B43CB", fontSize: 24 }}>
+                MEET OUR TEAM
+              </div>
+              <img
+                className="mt-3 ml-4"
+                src={Arrow}
+                alt="Arrow to meet the team"
+                style={{ width: "300px", maxWidth: "80%" }}
+              />
+            </Row>
           </Col>
-
-          <Col xs={12} md={3} className="">
+          <Col xs={12} md={4}>
             <img
-              src={Xinru}
-              alt="headshot of Xinru"
-              style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
+              src={TeamIllustration}
+              alt="Illustration for Team Page"
+              style={{ width: "350px", maxWidth: "100%" }}
             />
-            <div className="font-weight-bold pt-3">Xinru Li</div>
-            <div>Brown '22</div>
-            <div>Music</div>
-          </Col>
-          <Col xs={12} md={3} className="">
-            <img
-              src={Aryan}
-              alt="headshot of Aryan"
-              style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
-            />
-            <div className="font-weight-bold pt-3">Aryan Srivastava</div>
-            <div>Brown '22</div>
-            <div>CS</div>
           </Col>
         </Row>
       </Container>
       <Container
+        style={{
+          backgroundColor: "white",
+          borderRadius: "25px",
+          maxWidth: "90%",
+        }}
+      >
+        <Row className="pl-5 pt-3 pb-3" style={bodyStyle}>
+          Co-Chairs
+        </Row>
+        <Row className="d-flex pb-5 justify-content-start">
+          <Profile
+            name="Ivan Zhao"
+            src={Ivan}
+            year="Brown '22"
+            major="APMA - CS"
+          />
+          <Profile name="Xinru Li" src={Xinru} year="Brown '22" major="Music" />
+          <Profile
+            name="Aryan Srivastava"
+            src={Aryan}
+            year="Brown '22"
+            major="CS"
+          />
+        </Row>
+      </Container>
+      <Container
         className="mt-5 pb-5"
-        style={{ backgroundColor: "white", borderRadius: "25px", maxWidth: "90%" }}
+        style={{
+          backgroundColor: "white",
+          borderRadius: "25px",
+          maxWidth: "90%",
+        }}
       >
         <Row className="pl-5 pt-3 pb-3" style={bodyStyle}>
           E-BOARD
@@ -141,37 +150,24 @@ export default function index() {
           Curriculum
         </Row>
         <Row>
-          <Col xs={12} md={3}>
-            <img
-              src={Niyoshi}
-              alt="headshot of Niyoshi"
-              style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
-            />
-            <div className="font-weight-bold pt-3">Niyoshi Parekh</div>
-            <div>Brown '22</div>
-            <div>Architecture CS</div>
-          </Col>
-
-          <Col xs={12} md={3}>
-            <img
-              src={Brie}
-              alt="headshot of Brie"
-              style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
-            />
-            <div className="font-weight-bold pt-3">Brie Rowey</div>
-            <div>Brown '22</div>
-            <div>Mechanical Engineering</div>
-          </Col>
-          <Col xs={12} md={3}>
-            <img
-              src={Audrey}
-              alt="headshot of Audrey"
-              style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
-            />
-            <div className="font-weight-bold pt-3">Audrey Kang</div>
-            <div>RISD '22</div>
-            <div>Interior Architecture</div>
-          </Col>
+          <Profile
+            name="Niyoshi Parekh"
+            src={Niyoshi}
+            year="Brown '22"
+            major="Architecture CS"
+          />
+          <Profile
+            name="Brie Rowey"
+            src={Brie}
+            year="Brown '22"
+            major="Mechanical Engineering"
+          />
+          <Profile
+            name="Audrey Kang"
+            src={Audrey}
+            year="RISD '22"
+            major="Interior Architecture"
+          />
         </Row>
         <Row
           className="pl-5 pt-5 mb-3 font-weight-bold"
@@ -180,46 +176,30 @@ export default function index() {
           Marketing and Design
         </Row>
         <Row>
-          <Col xs={12} md={3}>
-            <img
-              src={Christine}
-              alt="headshot of Christine"
-              style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
-            />
-            <div className="font-weight-bold pt-3">Christine Lin</div>
-            <div>Brown '21</div>
-            <div>CS</div>
-          </Col>
-          <Col xs={12} md={3}>
-            <img
-              src={Kia}
-              alt="headshot of Kia"
-              style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
-            />
-            <div className="font-weight-bold pt-3">Kia Uusitalo</div>
-            <div>Brown '23</div>
-            <div>Organizational Studies</div>
-          </Col>
-          <Col xs={12} md={3}>
-            <img
-              src={Stephanie}
-              alt="headshot of Stephanie"
-              style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
-            />
-            <div className="font-weight-bold pt-3">Stephanie Park</div>
-            <div>RISD '22</div>
-            <div>Industrial Design</div>
-          </Col>
-          <Col xs={12} md={3}>
-            <img
-              src={Malvika}
-              alt="headshot of Malvika"
-              style={{ width: "175px", maxWidth: "80%", borderRadius: "50%" }}
-            />
-            <div className="font-weight-bold pt-3">Malvika Agarwal</div>
-            <div>RISD '22</div>
-            <div>Graphic Design</div>
-          </Col>
+          <Profile
+            name="Christine Lin"
+            src={Christine}
+            year="Brown '21"
+            major="CS"
+          />
+          <Profile
+            name="Kia Uusitalo"
+            src={Kia}
+            year="Brown '23"
+            major="Organizational Studies"
+          />
+          <Profile
+            name="Stephanie Park"
+            src={Stephanie}
+            year="RISD '22"
+            major="Industrial Design"
+          />
+          <Profile
+            name="Malvika Agarwal"
+            src={Malvika}
+            year="RISD '22"
+            major="Graphic Design"
+          />
         </Row>
 
         {/* <Row>Operations</Row> */}

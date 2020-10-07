a4b1a0ef5edae7aed5a69ef65e4f560386323680
SorinCho <sorincho@gmail.com>
2020-10-06 23:16:30 -0400

    Basic project template

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ import Footer from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <div className="App" style={{ minHeight: "100%" }}>
-            <Router>
-                <Switch>
-                    <Route path="/about-us" component={AboutUs} />
-                    <Route path="/contact" component={Contact} />
-                    <Route path="/calendar" component={Calendar} />
-                    <Route path="/curriculum" component={Curriculum} />
-                    <Route path="/projects" component={Projects} />
-                    <Route path="/" component={LandingPage} />
-                </Switch>
-            </Router>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App" style={{ minHeight: "100%" }}>
+      <Router>
+        <Switch>
+          <Route path="/about-us" component={AboutUs} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/calendar" component={Calendar} />
+          <Route path="/curriculum" component={Curriculum} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -6,59 +6,55 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Logo from "../../assets/new_logo.svg";
 
 const navStyle = {
-    fontFamily: "Arial",
-    fontSize: 24,
-    textTransform: "uppercase",
-    // color: "white",
+  fontFamily: "Arial",
+  fontSize: 24,
+  textTransform: "uppercase",
+  // color: "white",
 };
 
 export default function index({ linkFontColor }) {
-    return (
-        <div className="p-4" style={{ color: "white" }}>
-            <Navbar style={{ display: "flex" }} expand="lg">
-                <Navbar.Brand href="/" className="m-auto">
-                    <img
-                        style={{ width: "150px" }}
-                        src={Logo}
-                        alt="BRIC Default Logo"
-                    />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto mr-15" style={navStyle}>
-                        <Nav.Link
-                            href="/calendar"
-                            className="px-5"
-                            style={{ color: linkFontColor }}
-                        >
-                            Event Calendar
-                        </Nav.Link>
-                        <Nav.Link
-                            href="/projects"
-                            className="px-5"
-                            style={{ color: linkFontColor }}
-                        >
-                            Projects
-                        </Nav.Link>
-                        <Nav.Link
-                            href="/about-us"
-                            className="px-5"
-                            style={{ color: linkFontColor }}
-                        >
-                            About Us
-                        </Nav.Link>
-                        <Nav.Link
-                            href="/contact"
-                            className="px-5"
-                            style={{ color: linkFontColor }}
-                        >
-                            Contact
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-    );
+  return (
+    <div className="p-4" style={{ color: "white" }}>
+      <Navbar style={{ display: "flex" }} expand="lg">
+        <Navbar.Brand href="/" className="m-auto">
+          <img style={{ width: "150px" }} src={Logo} alt="BRIC Default Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto mr-15" style={navStyle}>
+            <Nav.Link
+              href="/calendar"
+              className="px-5"
+              style={{ color: linkFontColor }}
+            >
+              Event Calendar
+            </Nav.Link>
+            <Nav.Link
+              href="/projects"
+              className="px-5"
+              style={{ color: linkFontColor }}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href="/about-us"
+              className="px-5"
+              style={{ color: linkFontColor }}
+            >
+              About Us
+            </Nav.Link>
+            <Nav.Link
+              href="/contact"
+              className="px-5"
+              style={{ color: linkFontColor }}
+            >
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 // <Nav.Link

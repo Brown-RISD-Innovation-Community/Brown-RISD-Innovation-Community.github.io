4a860d7339a5b7b1df56de43891e54e79aae2c79
Ivan Zhao <ivanzhao8@gmail.com>
2021-01-11 23:30:46 -0800

    add applications

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -10,65 +10,76 @@ import NavigationBar from "../../components/NavigationBar";
 import LandingIllustration from "../../assets/landing.png";
 
 export function index() {
-  return (
-    <div
-      style={{
-        backgroundColor: "#0A44CB",
-        color: "white",
-        height: "100%",
-      }}
-    >
-      <NavigationBar linkFontColor="white" />
-      <Container
-        className="d-sm-flex justify-content-center"
-        style={{ height: "600px", alignItems: " center" }}
-      >
-        <Row>
-        <Col xs={12} md={4}>
-          <img
-            src={LandingIllustration}
-            alt="Illustration from Undraw on designers"
-            style={{ width: "350px", maxWidth: "100%" }}
-          />
-        </Col>
-        <Col xs={12} md={8}>
-          <div style={{ width: "600px", maxWidth: "100%" }}>
-            <Row
-              className="mb-2"
-              style={{
-                fontWeight: "bold",
-                fontSize: 30,
-                // width: "600px",
-                textTransform: "uppercase",
-                marginBottom: "",
-              }}
+    return (
+        <div
+            style={{
+                backgroundColor: "#0A44CB",
+                color: "white",
+                height: "100%",
+            }}
+        >
+            <NavigationBar linkFontColor="white" />
+            <Container
+                className="d-sm-flex justify-content-center"
+                style={{ height: "600px", alignItems: " center" }}
             >
-              Brown RISD Innovation Community
-            </Row>
-            <Row style={{ textAlign: "left" }} className="p-4">
-              EMPHASIZING THE IMPORTANCE OF INNOVATION IN FIELDS BEYOND PURE
-              TECHNOLOGY THROUGH CREATING AN INCLUSIVE SPACE FOR
-              INTERDISCIPLINARY COLLABORATION AND LEARNING.
-            </Row>
-            <Row className="p-4">
-              LEARN MORE
-              <img
-                className="mt-2 ml-4"
-                src={Arrow}
-                alt="Arrow to learn more"
-                style={{ width: "350px", maxWidth: "80%" }}
-              />
-            </Row>
-          </div>
-        </Col>
-        </Row>
-      </Container>
-      <Container
-        className="mb-5 mt-5"
-        style={{overflow:"hidden"}}
-        // style={{ position: "relative", top: "-30px", overflow:"hidden"}}
-      >
-        {/* <img
+                <Row>
+                    <Col xs={12} md={4}>
+                        <img
+                            src={LandingIllustration}
+                            alt="Illustration from Undraw on designers"
+                            style={{ width: "350px", maxWidth: "100%" }}
+                        />
+                    </Col>
+                    <Col xs={12} md={8}>
+                        <div style={{ width: "600px", maxWidth: "100%" }}>
+                            <Row
+                                className="mb-2"
+                                style={{
+                                    fontWeight: "bold",
+                                    fontSize: 30,
+                                    // width: "600px",
+                                    textTransform: "uppercase",
+                                    marginBottom: "",
+                                }}
+                            >
+                                Brown RISD Innovation Community
+                            </Row>
+                            <Row style={{ textAlign: "left" }} className="p-4">
+                                EMPHASIZING THE IMPORTANCE OF INNOVATION IN
+                                FIELDS BEYOND PURE TECHNOLOGY THROUGH CREATING
+                                AN INCLUSIVE SPACE FOR INTERDISCIPLINARY
+                                COLLABORATION AND LEARNING.
+                            </Row>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Container style={{ fontSize: "32px" }}>
+                Our applications for our spring{" "}
+                <a
+                    href="https://forms.gle/mxG7jhgNd4zMRbpE6"
+                    target="_blank"
+                    style={{ color: "yellow" }}
+                >
+                    E-board
+                </a>{" "}
+                and{" "}
+                <a
+                    href="https://forms.gle/ghtGzG1omfq4kCwo7"
+                    target="_blank"
+                    style={{ color: "yellow" }}
+                >
+                    cohorts
+                </a>{" "}
+                are open!
+            </Container>
+            <Container
+                className="mb-5 mt-5"
+                style={{ overflow: "hidden" }}
+                // style={{ position: "relative", top: "-30px", overflow:"hidden"}}
+            >
+                {/* <img
           src={Scissors}
           alt="Scissors for decoration"
           style={{
@@ -82,73 +93,80 @@ export function index() {
             zIndex: 0,
           }}
         /> */}
-        <div style={{ borderStyle: "dashed" }}>
-          <Row style={{ minHeight: "350px" }}>
-            <Col
-              xs={12}
-              md={4}
-              className="col-4 p-4"
-              style={{
-                fontWeight: "bold",
-                fontSize: 60,
-                textTransform: "uppercase",
-                margin: "auto",
-              }}
-            >
-              Vision
-            </Col>
-            <Col
-              xs={12}
-              md={8}
-              className="col-8"
-              style={{ textAlign: "left", margin: "auto" }}
-            >
-              <div className="p-4">
-              A DYNAMIC SPACE WHERE ARTISTS, HISTORIANS, COMPUTER SCIENTISTS,
-              DESIGNERS, AND EVERYONE IN BETWEEN CAN LEARN TOGETHER AND FROM
-              EACH OTHER THROUGH OUR INTEGRATED CURRICULUM. WE FOCUS ON
-              FOSTERING DIVERSE OPINIONS, LEARNING NEW SKILLS, AND HANDS ON
-              COLLABORATION ON COLLEGE HILL AND BEYOND.
-              </div>
-            </Col>
-          </Row>
+                <div style={{ borderStyle: "dashed" }}>
+                    <Row style={{ minHeight: "350px" }}>
+                        <Col
+                            xs={12}
+                            md={4}
+                            className="col-4 p-4"
+                            style={{
+                                fontWeight: "bold",
+                                fontSize: 60,
+                                textTransform: "uppercase",
+                                margin: "auto",
+                            }}
+                        >
+                            Vision
+                        </Col>
+                        <Col
+                            xs={12}
+                            md={8}
+                            className="col-8"
+                            style={{ textAlign: "left", margin: "auto" }}
+                        >
+                            <div className="p-4">
+                                A DYNAMIC SPACE WHERE ARTISTS, HISTORIANS,
+                                COMPUTER SCIENTISTS, DESIGNERS, AND EVERYONE IN
+                                BETWEEN CAN LEARN TOGETHER AND FROM EACH OTHER
+                                THROUGH OUR INTEGRATED CURRICULUM. WE FOCUS ON
+                                FOSTERING DIVERSE OPINIONS, LEARNING NEW SKILLS,
+                                AND HANDS ON COLLABORATION ON COLLEGE HILL AND
+                                BEYOND.
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
+            <Container className="pb-5">
+                <div style={{ borderStyle: "dashed" }}>
+                    <Row style={{ minHeight: "350px" }}>
+                        <Col
+                            xs={12}
+                            md={4}
+                            className="col-4 p-4"
+                            style={{
+                                fontSize: 60,
+                                textTransform: "uppercase",
+                                margin: "auto",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            Values
+                        </Col>
+                        <Col
+                            xs={12}
+                            md={8}
+                            className="col-8"
+                            style={{ margin: "auto" }}
+                        >
+                            <ul
+                                style={{
+                                    listStyle: "none",
+                                    textAlign: "left",
+                                    paddingInlineStart: "0",
+                                }}
+                                className="p-4"
+                            >
+                                <li>INCLUSIVE COMMUNITY</li>
+                                <li>INTERSECTIONAL INNOVATION</li>
+                                <li>EMBRACING FEEDBACK</li>
+                            </ul>
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
         </div>
-      </Container>
-      <Container className="pb-5">
-        <div style={{ borderStyle: "dashed" }}>
-          <Row style={{ minHeight: "350px" }}>
-            <Col
-              xs={12}
-              md={4}
-              className="col-4 p-4"
-              style={{
-                fontSize: 60,
-                textTransform: "uppercase",
-                margin: "auto",
-                fontWeight: "bold",
-              }}
-            >
-              Values
-            </Col>
-            <Col xs={12} md={8} className="col-8" style={{ margin: "auto" }}>
-              <ul
-                style={{
-                  listStyle: "none",
-                  textAlign: "left",
-                  paddingInlineStart: "0",
-                }}
-                className="p-4"
-              >
-                <li>INCLUSIVE COMMUNITY</li>
-                <li>INTERSECTIONAL INNOVATION</li>
-                <li>EMBRACING FEEDBACK</li>
-              </ul>
-            </Col>
-          </Row>
-        </div>
-      </Container>
-    </div>
-  );
+    );
 }
 
 export default index;

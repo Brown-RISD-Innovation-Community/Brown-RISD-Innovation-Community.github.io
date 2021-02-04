0d00b40716021fcfd030c9b2748e00c1242556d7
SorinCho <sorincho@gmail.com>
2021-02-04 17:40:28 -0500

    fall 2020 projects

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -3,138 +3,87 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Arrow from "../../assets/arrow.svg";
-import Scissors from "../../assets/scissors.svg";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+
 import NavigationBar from "../../components/NavigationBar";
 
-import LandingIllustration from "../../assets/landing.png";
+import SaltyCaramel from "../../assets/projects/salty_caramel.png";
+import DormLight from "../../assets/projects/dorm_light.jpg";
 
-const projects = [
-  {
-    name: "Project Name",
-    members: [
-      "April Goodman",
-      "Libbie Marriott",
-      "Duke Gates",
-      "Harry Harding",
-    ],
-    desc:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada sollicitudin augue, sit amet egestas eros malesuada varius. Ut eu enim sed urna ultricies condimentum in sit amet metus. Maecenas ipsum ex, luctus et cursus id, rhoncus vitae libero. Pellentesque venenatis facilisis est. Maecenas et dictum enim. Etiam pharetra eleifend velit, vel tempor ipsum scelerisque sit amet. Quisque congue quam purus, id vestibulum quam suscipit in. Donec a egestas quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas nibh augue, congue sit amet risus eu, ullamcorper luctus lectus. Ut sit amet luctus ipsum. Curabitur placerat nisl sapien, id ornare arcu rutrum eu. Curabitur vitae nibh eget est viverra tempor a et libero. Nam massa mauris, egestas non placerat dignissim, vehicula vel erat. Curabitur at convallis tortor, ac laoreet diam. Mauris venenatis at lorem at semper. Aenean turpis nisl, efficitur quis lorem bibendum, luctus maximus sem. Nam scelerisque elementum velit, sed vestibulum ligula varius vel. Fusce at lobortis est, placerat aliquet odio.",
-    src: LandingIllustration,
-  },
-  {
-    name: "Project Name",
-    members: [
-      "April Goodman",
-      "Libbie Marriott",
-      "Duke Gates",
-      "Harry Harding",
-    ],
-    desc:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada sollicitudin augue, sit amet egestas eros malesuada varius. Ut eu enim sed urna ultricies condimentum in sit amet metus. Maecenas ipsum ex, luctus et cursus id, rhoncus vitae libero. Pellentesque venenatis facilisis est. Maecenas et dictum enim. Etiam pharetra eleifend velit, vel tempor ipsum scelerisque sit amet. Quisque congue quam purus, id vestibulum quam suscipit in. Donec a egestas quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas nibh augue, congue sit amet risus eu, ullamcorper luctus lectus. Ut sit amet luctus ipsum. Curabitur placerat nisl sapien, id ornare arcu rutrum eu. Curabitur vitae nibh eget est viverra tempor a et libero. Nam massa mauris, egestas non placerat dignissim, vehicula vel erat. Curabitur at convallis tortor, ac laoreet diam. Mauris venenatis at lorem at semper. Aenean turpis nisl, efficitur quis lorem bibendum, luctus maximus sem. Nam scelerisque elementum velit, sed vestibulum ligula varius vel. Fusce at lobortis est, placerat aliquet odio.",
-    src: LandingIllustration,
-  },
-  {
-    name: "Project Name",
-    members: [
-      "April Goodman",
-      "Libbie Marriott",
-      "Duke Gates",
-      "Harry Harding",
-    ],
-    desc:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada sollicitudin augue, sit amet egestas eros malesuada varius. Ut eu enim sed urna ultricies condimentum in sit amet metus. Maecenas ipsum ex, luctus et cursus id, rhoncus vitae libero. Pellentesque venenatis facilisis est. Maecenas et dictum enim. Etiam pharetra eleifend velit, vel tempor ipsum scelerisque sit amet. Quisque congue quam purus, id vestibulum quam suscipit in. Donec a egestas quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas nibh augue, congue sit amet risus eu, ullamcorper luctus lectus. Ut sit amet luctus ipsum. Curabitur placerat nisl sapien, id ornare arcu rutrum eu. Curabitur vitae nibh eget est viverra tempor a et libero. Nam massa mauris, egestas non placerat dignissim, vehicula vel erat. Curabitur at convallis tortor, ac laoreet diam. Mauris venenatis at lorem at semper. Aenean turpis nisl, efficitur quis lorem bibendum, luctus maximus sem. Nam scelerisque elementum velit, sed vestibulum ligula varius vel. Fusce at lobortis est, placerat aliquet odio.",
-    src: LandingIllustration,
-  },
-  {
-    name: "Project Name",
-    members: [
-      "April Goodman",
-      "Libbie Marriott",
-      "Duke Gates",
-      "Harry Harding",
-    ],
-    desc:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada sollicitudin augue, sit amet egestas eros malesuada varius. Ut eu enim sed urna ultricies condimentum in sit amet metus. Maecenas ipsum ex, luctus et cursus id, rhoncus vitae libero. Pellentesque venenatis facilisis est. Maecenas et dictum enim. Etiam pharetra eleifend velit, vel tempor ipsum scelerisque sit amet. Quisque congue quam purus, id vestibulum quam suscipit in. Donec a egestas quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas nibh augue, congue sit amet risus eu, ullamcorper luctus lectus. Ut sit amet luctus ipsum. Curabitur placerat nisl sapien, id ornare arcu rutrum eu. Curabitur vitae nibh eget est viverra tempor a et libero. Nam massa mauris, egestas non placerat dignissim, vehicula vel erat. Curabitur at convallis tortor, ac laoreet diam. Mauris venenatis at lorem at semper. Aenean turpis nisl, efficitur quis lorem bibendum, luctus maximus sem. Nam scelerisque elementum velit, sed vestibulum ligula varius vel. Fusce at lobortis est, placerat aliquet odio.",
-    src: LandingIllustration,
-  },
-];
-function DottedBox(props) {
-  return (
-    <Container className="py-5">
-      <div style={{ borderStyle: "dashed" }}>
-        <Row style={{ minHeight: "350px" }}>
-          <Col
-            xs={12}
-            md={4}
-            className="col-4 p-4"
-            style={{
-              fontWeight: "bold",
-              fontSize: 60,
-              textTransform: "uppercase",
-              margin: "auto",
-            }}
-          >
-            {props.cardTitle}
-          </Col>
-          <Col
-            xs={12}
-            md={8}
-            className="col-8"
-            style={{ textAlign: "left", margin: "auto" }}
-          >
-            {props.children}
-          </Col>
-        </Row>
-      </div>
-    </Container>
-  );
-}
+// const projects = [
+//   {
+//     name: "Salty Caramel",
+//     members: [
+//       "Jasmine Shum",
+//       "Jennifer Wang",
+//       "Felicia Renelus",
+//       "Elizabeth Wu",
+//       "Kevin Hsu",
+//     ],
+//     desc: "An app that aggresively reminds you to complete your tasks",
+//     src: SaltyCaramel,
+//     link: "https://github.com/kaisucode/salty-caramel",
+//   },
+//   {
+//     name: "Dorm Light Exploration",
+//     members: ["Naveen Upender", "Lucia Tian", "Connie Liu", "Eiden Spilker"],
+//     desc:
+//       "A series of concepts and ideas for dorm lights fitting different needs and purposes",
+//     src: SaltyCaramel,
+//     link:
+//       "https://docs.google.com/presentation/d/1Gb7iQ9xp7kTHTq6S-3R4iYXj66qL_K9ZsjPutEnumq4/edit?usp=sharing",
+//   },
+// ];
 
-function Project({ name, src, desc, members }) {
-  console.log(members);
-  return (
-    <Container className="py-3">
-      <div style={{ borderStyle: "dashed" }}>
-        <Row className="px-5 pt-5">
-          <Col xs={{ span: 12, order: 2 }} md={{ span: 6, order: 1 }}>
-            <Row>
-              <h3>{name}</h3>
-            </Row>
-            <Row>
-              <Col className="text-left">
-                <h5>Team Members</h5>
-              </Col>
-              {members.map((member) => (
-                <Col className="text-left" xs={12}>
-                  {member}
-                </Col>
-              ))}
-            </Row>
-          </Col>
-          <Col xs={{ span: 12, order: 1 }} md={{ span: 6, order: 2 }}>
-            <img src={src} alt="project image" style={{ maxWidth: "100%" }} />
-          </Col>
-        </Row>
-        <Row className="px-5 pb-5 pt-3 text-left">
-          <h5>About</h5>
-          {desc}
-        </Row>
-      </div>
-    </Container>
-  );
-}
+// function Project({ name, src, desc, members, link }) {
+//   console.log(members);
+//   return (
+//     <Container className="py-3">
+//       <div style={{ borderStyle: "solid" }}>
+//         <Row className="px-5 pt-5">
+//           <Col xs={{ span: 12, order: 2 }} md={{ span: 6, order: 1 }}>
+//             <Row>
+//               <h3>{name}</h3>
+//             </Row>
+//             <Row>
+//               <Col className="text-left">
+//                 <h5>Team Members</h5>
+//               </Col>
+//               {members.map((member) => (
+//                 <Col className="text-left" xs={12}>
+//                   {member}
+//                 </Col>
+//               ))}
+//             </Row>
+//           </Col>
+//           <Col xs={{ span: 12, order: 1 }} md={{ span: 6, order: 2 }}>
+//             <img src={src} alt={name} style={{ maxWidth: "100%" }} />
+//           </Col>
+//         </Row>
+//         <Row className="px-5 pb-5 pt-3 text-left">
+//           <Col>
+//             <h5>About</h5>
+//             <p>{desc}</p>
+//             <a href={link}>Link</a>
+//           </Col>
+//         </Row>
+//       </div>
+//     </Container>
+//   );
+// }
 export function index() {
   return (
     <div
       style={{
-        backgroundColor: "#0A44CB",
+        backgroundColor: "#8E1DE6",
         color: "white",
         height: "100%",
       }}
+      className="pb-5"
     >
       <NavigationBar linkFontColor="white" />
-      <Container
+      {/* <Container
         className="d-sm-flex justify-content-center"
         style={{ height: "600px", alignItems: " center" }}
       >
@@ -175,15 +124,66 @@ export function index() {
             </div>
           </Col>
         </Row>
+      </Container> */}
+      <Container className="d-sm-flex justify-content-center">
+        <Row>
+          <Col>
+            <div className="m-4" style={{ fontSize: "48px", margin: "5px" }}>
+              PROJECTS
+            </div>
+          </Col>
+        </Row>
       </Container>
-      {projects.map((project) => (
+      {/* {projects.map((project) => (
         <Project
           name={project.name}
           src={project.src}
           desc={project.desc}
           members={project.members}
         />
-      ))}
+      ))} */}
+      <Container
+        style={{ color: "black", width: "700px", maxWidth: "100%" }}
+        className="d-sm-flex justify-content-center flex-column"
+      >
+        <h2 className="mb-5" style={{ color: "white" }}>
+          Fall 2020
+        </h2>
+        <CardDeck>
+          <Card style={{ width: "100%" }}>
+            <Card.Img variant="top" src={SaltyCaramel} />
+            <Card.Body>
+              <Card.Title>Salty Caramel</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Jasmine Shum, Jennifer Wang, Felicia Renelus, Elizabeth Wu,
+                Kevin Hsu
+              </Card.Subtitle>
+              <Card.Text className="mt-2">
+                An app that aggresively reminds you to complete your tasks
+              </Card.Text>
+              <Card.Link href="https://github.com/kaisucode/salty-caramel">
+                Github
+              </Card.Link>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "100%" }}>
+            <Card.Img variant="top" src={DormLight} />
+            <Card.Body>
+              <Card.Title>Dorm Light Exploration</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Naveen Upender, Lucia Tian, Connie Liu, Eiden Spilker
+              </Card.Subtitle>
+              <Card.Text className="mt-2">
+                A series of concepts and ideas for dorm lights fitting different
+                needs and purposes
+              </Card.Text>
+              <Card.Link href="https://docs.google.com/presentation/d/1Gb7iQ9xp7kTHTq6S-3R4iYXj66qL_K9ZsjPutEnumq4/edit?usp=sharing">
+                Google Slides
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+      </Container>
     </div>
   );
 }

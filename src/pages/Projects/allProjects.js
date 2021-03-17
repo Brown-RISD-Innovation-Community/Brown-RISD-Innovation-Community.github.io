b17abf824cc7d51e23900551527b1952733ddba4
SorinCho <sorincho@gmail.com>
2021-03-17 00:52:50 -0400

    all projects data file

---
+++ src/pages/Projects/allProjects.js
@@ -0,0 +1,31 @@
+import SaltyCaramel from "../../assets/projects/salty_caramel.png";
+import DormLight from "../../assets/projects/dorm_light.jpg";
+
+const allProjects = [
+  {
+    semester: "Fall 2020",
+    projects: [
+      {
+        title: "Salty Caramel",
+        src: SaltyCaramel,
+        members:
+          "Jasmine Shum, Jennifer Wang, Felicia Renelus, Elizabeth Wu, Kevin Hsu",
+        description:
+          "An app that aggresively reminds you to complete your tasks",
+        link: "https://github.com/kaisucode/salty-caramel",
+        linkType: "GitHub",
+      },
+      {
+        title: "Dorm Light Exploration",
+        src: DormLight,
+        members: "Naveen Upender, Lucia Tian, Connie Liu, Eiden Spilker",
+        description:
+          "A series of concepts and ideas for dorm lights fitting different needs and purposes",
+        link:
+          "https://docs.google.com/presentation/d/1Gb7iQ9xp7kTHTq6S-3R4iYXj66qL_K9ZsjPutEnumq4/edit?usp=sharing",
+        linkType: "Google Slides",
+      },
+    ],
+  },
+];
+export default allProjects;
